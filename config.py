bf63b4145752ccef71989445355789c1dcfb9c7f
Filip <filip.mazur123@gmail.com>
2024-04-06 12:27:02 +0200

    Text classification using transformers
    - own training loop and transformers.Trainer

---
+++ config.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+# path to the SDK folder
+SDK_PATH: Optional[str] = 'D:\\Studia\\magisterskie\\Praca_magisterska\\data\\repo\\CMU-MultimodalSDK'
+
+# path to a pretrained word embedding file
+WORD_EMB_PATH: Optional[str] = 'D:\\Studia\\magisterskie\\Praca_magisterska\\eksperymenty\\projekt\\glove\\glove.840B.300d.txt'
+
+# path to loaded word embedding matrix and corresponding word2id mapping
+CACHE_PATH: Optional[str] = 'D:\\Studia\\magisterskie\\Praca_magisterska\\eksperymenty\\projekt\\data\\embedding_and_mapping.pt'
+
+# path to the folder where you want to store data
+# DATA_PATH: Optional[str] = 'D:\\Studia\\magisterskie\\Praca_magisterska\\eksperymenty\\projekt\\data\\'
+DATA_PATH: Optional[str] = 'D:/Studia/magisterskie/Praca_magisterska/eksperymenty/projekt/data/'
